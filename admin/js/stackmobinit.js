62bba2752ba3c4d1a7ae42041d6ed6df4c8e354a
blackmwana <blackmwana@gmail.com>
2013-03-01 04:50:27 -0500

    mviro mviro

---
+++ admin/js/stackmobinit.js
@@ -0,0 +1,7 @@
+ StackMob.init({
+    			appName: "afrykashop",
+				clientSubdomain: "blackmwanagmailcom",
+				publicKey: "be36fa6a-32de-41fe-873e-eca9169850d2",
+				apiVersion: 0
+			});
+         //   alert("stackmob is ready baas");
