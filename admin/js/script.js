fc34d259ebb7b115d89e281f37e869fc53a97628
blackmwana <blackmwana@gmail.com>
2013-03-16 13:49:36 -0400

    product page

--- admin/js/script.js
+++ admin/js/script.js
@@ -788,7 +788,7 @@ $(document).ready(function() {
                 // Read in the file as a data URL
                 var fileContent = reader.readAsDataURL(f);
                 console.debug(fileContent);
-                console.debug(fileContent.result);
+                console.debug(reader.result);
                  $('.product-modal-img').attr({src:fileContent}).addClass('image-updated');
             }
         },
