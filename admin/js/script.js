112a3dfd7c167f44ec60fa72e0e98bd7e7d6f8b9
blackmwana <blackmwana@gmail.com>
2013-04-01 16:08:14 -0400

    product page

--- admin/js/script.js
+++ admin/js/script.js
@@ -911,7 +911,7 @@ $(document).ready(function() {
             console.debug(checked);
         },
         filterSelects:function(el,checked){
-            /console.debug(this);
+            // console.debug(this);
             //console.debug(this.parent);
             //console.debug(el.attr('class'));
           //  window.pusy=el;
